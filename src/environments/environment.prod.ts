0ee5ca603a510e852b662b2ba07fdcebf4aa33a2
Juan Carlos Alban luna <juancarlos-197@unimayor.edu.co>
2025-06-11 14:30:39 -0500

    Categorias Ionic Pruebas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+   firebaseConfig : {
+  apiKey: "AIzaSyC7DkS2K0ESk_tOeWFuZF15qYLr24t4s60",
+  authDomain: "categorias-admin-app.firebaseapp.com",
+  projectId: "categorias-admin-app",
+  storageBucket: "categorias-admin-app.firebasestorage.app",
+  messagingSenderId: "276747268511",
+  appId: "1:276747268511:web:65487dbf61afd5098f4daa"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyC7DkS2K0ESk_tOeWFuZF15qYLr24t4s60",
+  authDomain: "categorias-admin-app.firebaseapp.com",
+  projectId: "categorias-admin-app",
+  storageBucket: "categorias-admin-app.firebasestorage.app",
+  messagingSenderId: "276747268511",
+  appId: "1:276747268511:web:65487dbf61afd5098f4daa"
+}
+
 };
 
 /*
